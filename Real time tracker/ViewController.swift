1977c4f376f8b3ad521e22e5d813546d62048177
Khawar Ali <khawarali5@gmail.com>
2021-05-09 18:24:02 +0500

    first commit

---
+++ Real time tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Real time tracker
+//
+//  Created by khawar on 5/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
