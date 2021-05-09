fc8435d88be095a738be60b311497b1d7401bfbd
Khawar Ali <khawar.ali@brainxtech.com>
2021-05-09 21:06:59 +0500

    some changes

--- Real time tracker/Views/MapViewController.swift
+++ Real time tracker/Views/MapViewController.swift
@@ -48,18 +48,14 @@ class MapViewController: UIViewController {
     
     func startTrackingUser() {
         if let trackedUserId = trackedUser["id"] as? String {
-            
-            //Send to server a message that user is now tracking a tracked user location
             LocationSocketManager.sharedInstance.userStartedTracking(trackedUserSocketId: trackedUserId, coordinatesUpdateHandler: { trackedUsersCoordinatesUpdate in
                 
-                //When we receive tracked user current location the map is updated
                 if let latitudeString = trackedUsersCoordinatesUpdate?["latitude"] as? String, let longitudeString = trackedUsersCoordinatesUpdate?["longitude"] as? String, let latitude = Double(latitudeString), let longitude = Double(longitudeString) {
                     self.updateTrackedUserLocation(withLatitude: latitude, andLongitude: longitude)
                 }
                 
             }, trackedUserStoppedTrackingHandler: { trackedUserNickname in
                 
-                //When the tracked user stops sharing location we return to the tracked users list
                 if let nickname = trackedUserNickname {
                     let alert = UIAlertController(title: "Ops!", message: "\(nickname) has stopped sharing location", preferredStyle: .alert)
                     let okButton = UIAlertAction(title: "Ok", style: .default) { action in
@@ -77,7 +73,6 @@ class MapViewController: UIViewController {
     
     func stopTrackingUser() {
         if let trackedUserId = trackedUser["id"] as? String {
-            //Let the server know that the user is no longer tracking
             LocationSocketManager.sharedInstance.userStoppedTracking(trackedUserSocketId: trackedUserId)
         }
     }
